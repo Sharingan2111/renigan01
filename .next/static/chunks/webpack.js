c9f76f4a072dfdf2fda90dd9f4858be996fb3a6e
Sharingan2111 <andrey020066@mail.ru>
2025-07-17 15:44:04 +0300

    feat: зафиксировал текущую рабочую версию (как на localhost:3000)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "23924adff12f8524"; }
+/******/ 		__webpack_require__.h = function() { return "9e7a06dc52488db4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
